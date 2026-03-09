fa3794983b5667d5bc56361421121eb55f44967c
nkosana101 <tech101school@gmail.com>
2026-03-09 22:58:34 +0200

    commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Ask for first name
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        // Ask for last name
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        // Display the result in the specified format
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
