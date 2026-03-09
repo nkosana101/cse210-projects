c4549980470304bd2a1f4d2a21b269b520cd57a0
nkosana101 <tech101school@gmail.com>
2026-03-09 22:47:05 +0200

    commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string playAgain = "yes";
+
+        // Stretch challenge: loop to play again
+        while (playAgain == "yes")
+        {
+            // Core requirement 3: random number from 1 to 100
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;   // Stretch challenge: count guesses
+
+            // Core requirement 2: keep looping until guessed correctly
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // Stretch challenge: display number of guesses
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            // Stretch challenge: ask to play again
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
